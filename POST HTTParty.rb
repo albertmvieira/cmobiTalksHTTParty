04948c43d0e13ca0ae63b5960115a8057ae1c7e2
rafael.bercam <rafael.bercam@conta.mobi>
2018-08-29 18:11:46 -0300

    update

--- POST HTTParty.rb
+++ POST HTTParty.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 response = HTTParty.post('http://localhost:3000/bank',
-    :body => {"code":"004","name": "Caixa Economica"})
+    :body => {"code": 237,"name": "Banco Bradesco S.A"})
 puts "response code: #{response.code}"
 puts "response message: #{response.message}"
 puts "response headers: #{response.headers}"
